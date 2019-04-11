9cfb7aaabf99d8cf0b6398154f73da8d4d8096f4
Salman Tariq Lone <salmanlone89@gmail.com>
2019-04-11 16:44:38 +0800

    FIrst checkin

---
+++ Hello/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(CreateMessage());
+		}
+
+		public static string CreateMessage()
+		{
+			return "Hello World!";
+		}
+	}
+}
---
+++ HelloTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelloTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelloTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f00b3150-835c-4e10-9b48-14ce3a97ff63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
